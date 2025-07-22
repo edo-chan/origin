5723480a1c453e1e3e8dd97a419f15574bb5487f
Edward Chan <edward@sqds.io>
2025-07-21 22:03:10 -0400

    Remove unnecessary files and add .gitignore

--- target/debug/deps/template-d4b81a7183657902.d
+++
@@ -1,5 +0,0 @@
-/Users/edwardchan/dev/template/target/debug/deps/template-d4b81a7183657902.d: src/main.rs
-
-/Users/edwardchan/dev/template/target/debug/deps/libtemplate-d4b81a7183657902.rmeta: src/main.rs
-
-src/main.rs:
